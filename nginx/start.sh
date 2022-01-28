246ca9727faf99a214f436ff55753b56938378f0
enriquepablo <enrique@cazalla.net>
2022-01-28 17:24:07 +0100

    use X-Real-IP for the client IP in shibboleth

--- nginx/start.sh
+++ nginx/start.sh
@@ -204,9 +204,6 @@ http {
       server_name ${SP_HOSTNAME};
       #root /opt/public;
 
-      set_real_ip_from ${PROXY_NETWORK-${SP_HOSTNAME}};
-      real_ip_header X-Forwarded-For;
-
       ssl_certificate ${KEYDIR}/certs/${CERTNAME}.crt;
       ssl_certificate_key ${KEYDIR}/private/${CERTNAME}.key;
       ssl_session_timeout 1d;
