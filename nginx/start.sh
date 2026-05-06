46913aa67a997517a6f8c5d3e33442eb7a0f94a0
Enrique Pérez Arnaud <enrique@cazalla.net>
2026-05-06 16:00:46 +0200

    adding the eduSign webapp to the docker compose environment - wip

--- nginx/start.sh
+++ nginx/start.sh
@@ -148,7 +148,10 @@ cat>/etc/shibboleth/shibboleth2.xml<<EOF
             <MetadataFilter type="Signature" certificate="${MDQ_SIGNER_CERT}" />
         </MetadataProvider>
         <!-- Local BankID metadata. -->
-        <MetadataProvider type="XML" validate="true" path="${BANKID_MD_PATH}" reloadChanges="true">
+        <MetadataProvider type="MDQ" validate="true" cacheDirectory="mdq"
+	            baseUrl="${MDQ_BASE_URL_EID}" ignoreTransport="true">
+            <MetadataFilter type="RequireValidUntil" maxValidityInterval="2419200"/>
+            <MetadataFilter type="Signature" certificate="${MDQ_SIGNER_CERT_EID}" />
         </MetadataProvider>
         <!-- Map to extract attributes from SAML assertions. -->
         <AttributeExtractor type="XML" validate="true" reloadChanges="false" path="attribute-map.xml"/>
